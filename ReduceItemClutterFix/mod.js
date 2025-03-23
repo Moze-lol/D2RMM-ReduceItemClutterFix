1544582db59ae9fdb0454b82cc4df2f2169ec042
William Valla <mozeintel@gmail.com>
2025-03-23 04:21:08 +0100

    Initial upload

---
+++ ReduceItemClutterFix/mod.js
@@ -0,0 +1,40 @@
+//Example mod tested to "Works on my machine" levels on D2R.exe version 1.6.84219 and D2RMM 1.7.3
+//D2R.exe was run with -seed 494790446, heading straight to Lower Kurast waipoint and opening the first super chest in the hut directly south.
+//I'm a C++ developer, so take this JS code as a proof of concept. It is is no way optimized.
+
+const fileName = "global\\excel\\treasureclassex.txt";
+let file = D2RMM.readTsv(fileName);
+
+//I didn't bother setting up D2RMM configurations, so I just hardcoded a working example.
+const TCS_TO_REMOVE = new Set( 
+[
+    "gld", //Small gold piles
+    "hp1", "hp2", "hp3", "hp4", "mp1", "mp2", "mp3", "mp4", //Mana & Healing potions < Super
+    "rvs", "yps", "vps", "wms", //Small Rejuvs & Utility Potions
+    "gps", "ops", "gpm", "gpl", "opm", "gpl", "opl", //Throwing Potions
+    "gcv", "gcw", "gcg", "gcr", "gcb", "skc", "gcy", //Chipped Gems
+    "gfv", "gfw", "gfg", "gfr", "gfb", "skf", "gfy", //Flawed Gems
+    "gsv", "gsw", "gsg", "gsr", "gsb", "sku", "gsy", //Standard Gems
+    "tsc", "isc", //Town/ID scrolls,
+    "aqv", "cqv", //Arrows & bolts
+    "key", //Keys
+]);
+
+file.rows.forEach((row) => 
+{
+    //Not sure if necessary, in some of my tests runs modifying the "Gold" TC caused issues. More testing recommended.
+    if (row["Treasure Class"] == "Gold") 
+    {
+        return;
+    }
+
+    for (let i = 1; i <= 10; i++)
+    {
+        if (TCS_TO_REMOVE.has(row[`Item${i}`]))
+        {
+            row[`Item${i}`] = "gld";
+        }
+    }
+});
+
+D2RMM.writeTsv(fileName, file);
